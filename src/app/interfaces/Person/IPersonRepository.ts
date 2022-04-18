d742625b148b7d3fd519126ed81b93f7b3294d8b
Brendson <brendson.net@gmail.com>
2022-04-18 14:26:14 -0300

    feat: created the route to delete person

--- src/app/interfaces/Person/IPersonRepository.ts
+++ src/app/interfaces/Person/IPersonRepository.ts
@@ -5,4 +5,5 @@ export interface IPersonRepository {
   read: (payload) => Promise<IPerson | IPerson[]>
   readID: (id: string) => Promise<IPerson>
   update: (id: string, payload: IPerson) => Promise<IPerson>
+  delete: (id: string) => Promise<IPerson>
 }
--- src/app/interfaces/Person/IPersonService.ts
+++ src/app/interfaces/Person/IPersonService.ts
@@ -5,4 +5,5 @@ export interface IPersonService {
   read: (payload) => Promise<IPerson | IPerson[]>
   readID: (id: string) => Promise<IPerson>
   update: (id: string, payload: IPerson) => Promise<IPerson>
+  delete: (id: string) => Promise<IPerson>
 }
