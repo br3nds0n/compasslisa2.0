e396e6361907d47fdf67bb07f6b610ee7599f89e
Brendson <brendson.net@gmail.com>
2022-04-22 09:03:40 -0300

    ref: added constructor in repository classes

--- src/app/repository/CarRepository.ts
+++ src/app/repository/CarRepository.ts
@@ -7,38 +7,44 @@ import { ICarRepository } from '../interfaces/Car/ICarRepository';
 
 @Injectable()
 class CarRepository implements ICarRepository {
+  private carRepository: typeof CarModel;
+
+  constructor() {
+    this.carRepository = CarModel;
+  }
+
   async create(car: ICar): Promise<ICar> {
-    const NEW_CAR = await CarModel.create(car);
+    const NEW_CAR = await this.carRepository.create(car);
 
     return NEW_CAR;
   }
 
   async read(payload): Promise<ICar | ICar[]> {
-    const ALL_CARS = await CarModel.find(payload);
+    const ALL_CARS = await this.carRepository.find(payload);
 
     return ALL_CARS;
   }
 
   async readID(id: string): Promise<ICar> {
-    const ID_CAR = await CarModel.findById(id);
+    const ID_CAR = await this.carRepository.findById(id);
 
     return ID_CAR;
   }
 
   async update(id: string, NewBody: ICar): Promise<ICar> {
-    const NEW_BODY = await CarModel.findByIdAndUpdate(id, NewBody);
+    const NEW_BODY = await this.carRepository.findByIdAndUpdate(id, NewBody);
 
     return NEW_BODY;
   }
 
   async delete(id: string): Promise<ICar> {
-    const DELETE_CAR = await CarModel.findByIdAndDelete(id);
+    const DELETE_CAR = await this.carRepository.findByIdAndDelete(id);
 
     return DELETE_CAR;
   }
 
   async updateAccessory(id: string, accessoryId: string, payload): Promise<ICar> {
-    const NEW_ACCESSORY = await CarModel.findByIdAndUpdate(
+    const NEW_ACCESSORY = await this.carRepository.findByIdAndUpdate(
       id,
       { $set: { 'acessorios.$[outer].descricao': payload.descricao } },
       { arrayFilters: [{ 'outer._id': accessoryId }] },
